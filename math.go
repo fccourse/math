61124dccbc8557dad21d0e5b66120330a4f7e0b7
mrl00 <m2l_0@outlook.com>
2021-10-22 13:23:01 -0300

    refactor: rename sum function

--- math.go
+++ math.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println(Soma(10, 10))
+  fmt.Println(Sum(10, 10))
 }
 
-func Soma(a int, b int) int {
+func Sum(a int, b int) int {
   return a + b
 }
 
