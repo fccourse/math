7ccf8c785c3a9ccb2e3955f5a031cbaaca5501ac
mrl00 <m2l_0@outlook.com>
2021-10-22 13:28:46 -0300

    test: add tests for operations
    add test for sub, mul and div operations

--- math_test.go
+++ math_test.go
@@ -8,3 +8,26 @@ func TestSum(t *testing.T) {
     t.Errorf("Result must be 30 not %d", total)
   }
 }
+
+func TestSub(t *testing.T) {
+  total := Sub(15, 15)
+  if total != 0 {
+    t.Errorf("Result must be 0 not %d", total)
+  }
+}
+
+
+func TestMul(t *testing.T) {
+  total := Mul(15, 15)
+  if total != 225 {
+    t.Errorf("Result must be 225 not %d", total)
+  }
+}
+
+func TestDiv(t *testing.T) {
+  total := Sum(15, 15)
+  if total != 1 {
+    t.Errorf("Result must be 1 not %d", total)
+  }
+}
+
