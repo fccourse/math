c0745053d950c5ce2faf190cf979580688fed310
mrl00 <m2l_0@outlook.com>
2021-10-22 13:28:04 -0300

    feat: add new operations
    add sub, mult and div operations

--- math.go
+++ math.go
@@ -10,3 +10,14 @@ func Sum(a int, b int) int {
   return a + b
 }
 
+func Sub(a int, b int) int {
+  return a - b
+}
+
+func Mul(a int, b int) int {
+  return a * b
+}
+
+func Div(a int, b int) int {
+  return a / b
+}
